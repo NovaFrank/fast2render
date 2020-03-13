b4d28dfe6fd6d39864c5dd6660ed83e2db42cb65
李书群 <qunlee_mr@163.com>
2020-03-13 16:52:08 +0800

    代码规范

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,11 @@ module.exports = {
     port: 8080,
     disableHostCheck: true,
     proxy: {
-      '/apis': { // 这里最好有一个/
-        target: 'http://cs.51qqt.com/qqt-srm',  // 后台接口域名
-        secure: false,  // 如果是https接口，需要配置这个参数
-        changeOrigin: true,  // 是否跨域
+      '/apis': {
+        // 这里最好有一个/
+        target: 'http://cs.51qqt.com/qqt-srm', // 后台接口域名
+        secure: false, // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 是否跨域
         pathRewrite: {
           '^/apis': ''
         }

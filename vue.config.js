db96918964aebfdaf5250c1763ff712f90ce14e3
李书群 <qunlee_mr@163.com>
2020-03-13 16:52:01 +0800

    代码规范

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  //路径前缀
-  publicPath: "./",
+  // 路径前缀
+  publicPath: './',
   lintOnSave: true,
   productionSourceMap: false,
   devServer: {
     port: 8080,
     disableHostCheck: true,
     proxy: {
-      '/apis': {     //这里最好有一个 /
+      '/apis': { // 这里最好有一个/
         target: 'http://cs.51qqt.com/qqt-srm',  // 后台接口域名
         secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true,  // 是否跨域
-        pathRewrite:{
+        pathRewrite: {
           '^/apis': ''
         }
       }
     }
   }
-}
+};

a84df1ec7bb0a9e13f2183a962bf83485af5aab8
李书群 <qunlee_mr@163.com>
2020-03-19 14:18:44 +0800

    代码格式化

--- vue.config.js
+++ vue.config.js
@@ -9,11 +9,11 @@ module.exports = {
     proxy: {
       '/apis/': {
         // 这里最好有一个/
-        target: 'http://cs.51qqt.com/qqt-srm/rest', // 后台接口域名
+        target: 'http://cs.51qqt.com/ELSServer_SRM/rest', // 后台接口域名
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 是否跨域
         pathRewrite: {
-          '^/apis/': ''
+          '^/apis/': '/'
         }
       }
     }

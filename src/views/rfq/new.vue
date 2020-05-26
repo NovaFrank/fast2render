15be1a7325205d20e84f5eb22436426e1d29fb28
李书群 <qunlee_mr@163.com>
2020-05-26 17:06:20 +0800

    73730

--- src/views/rfq/new.vue
+++ src/views/rfq/new.vue
@@ -366,6 +366,13 @@ export default {
                   this.$message.error('请添加询价明细');
                   return;
                 }
+                let validate = this.inquiryListOption.data.filter(
+                  (item) => validatenull(item.quoteMethod) || validatenull(item.taxRate)
+                );
+                if (validate.length > 0) {
+                  this.$message.error('请完善报价方式或税码/税率');
+                  return;
+                }
                 let params = {
                   enquiryNumber: this.currentEnquiryNumber,
                   elsAccount: this.elsAccount,

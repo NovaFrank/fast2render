f9d977cd422f141a17a468df8d4b405422dc6c6c
Fan <fansc@shinetechchina.com>
2020-05-31 10:39:59 +0800

    问题修复： 订单 微服务调用优化处理

--- src/main.js
+++ src/main.js
@@ -1,27 +1,73 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+import routes from './router';
 import ElementUI from 'element-ui';
-
+import store from './store';
+import './public-path';
+import i18n from '../locales';
 import Avue from '@smallwei/avue';
-import fast2render from 'fast2render';
 import './styles/common.scss';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@smallwei/avue/lib/index.css';
+import Fast2Render from 'fast2render';
 import basicContainer from './components/basic-container/main';
-import md5 from 'js-md5';
+import { getCommonRes } from '@/util/commonRes';
+
+Vue.use(Fast2Render);
 
 Vue.use(ElementUI);
 Vue.use(Avue);
-Vue.use(fast2render);
-Vue.prototype.$md5 = md5;
 
 // 注册全局容器
 Vue.component('basicContainer', basicContainer);
-
+Vue.prototype.$GetCommon = getCommonRes;
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: (h) => h(App)
-}).$mount('#app');
+let router = null;
+let instance = null;
+
+function storeTest(props) {
+  props.onGlobalStateChange &&
+    props.onGlobalStateChange(
+      (value, prev) => console.log(`[onGlobalStateChange - ${props.name}]:`, value, prev),
+      true
+    );
+  props.setGlobalState &&
+    props.setGlobalState({
+      ignore: props.name,
+      user: {
+        name: props.name
+      }
+    });
+}
+
+function render(props = {}) {
+  const { container } = props;
+  router = routes;
+  instance = new Vue({
+    router,
+    store,
+    i18n,
+    render: (h) => h(App)
+  }).$mount(container ? container.querySelector('#app') : '#app');
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log('[vue] vue app bootstraped');
+}
+
+export async function mount(props) {
+  console.log('[vue] props from main framework', props);
+  storeTest(props);
+  render(props);
+}
+
+export async function unmount() {
+  instance.$destroy();
+  instance = null;
+  router = null;
+}
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,29 @@
+const path = require('path');
+const { name } = require('./package');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
   // 路径前缀
   publicPath: './',
   lintOnSave: true,
   productionSourceMap: false,
+  chainWebpack: (config) => {
+    // 忽略的打包文件
+    config.externals({
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      avue: 'Avue',
+      'vue-i18n': 'VueI18n',
+      'element-ui': 'ELEMENT'
+    });
+  },
   devServer: {
+    port: 8087,
     proxy: {
       '/apis/': {
         // target: 'http://cs.51qqt.com/ELSServer_SRM',
@@ -23,6 +43,24 @@ module.exports = {
           '^/layout/': '/'
         }
       }
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'no-cache'
+    }
+  },
+  // 自定义webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    },
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${name}`
     }
   }
 };

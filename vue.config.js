bac62d6827b6fa6acdde8283f41f5b7e69fa9f38
李书群 <qunlee_mr@163.com>
2020-06-01 10:31:06 +0800

    vue.config.js 变更

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,27 @@
+const path = require('path');
+const { name } = require('./package');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
   // 路径前缀
   publicPath: './',
   lintOnSave: true,
   productionSourceMap: false,
+  chainWebpack: (config) => {
+    // 忽略的打包文件
+    config.externals({
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      avue: 'Avue',
+      'vue-i18n': 'VueI18n',
+      'element-ui': 'ELEMENT'
+    });
+  },
   devServer: {
     port: 8080,
     disableHostCheck: true,
@@ -24,6 +43,24 @@ module.exports = {
           '^/layout/': '/'
         }
       }
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'no-cache'
+    }
+  },
+  // 自定义webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    },
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${name}`
     }
   }
 };

e6aa4190d3ef25dc93fd185622f8f4a765843403
李书群 <qunlee_mr@163.com>
2020-07-01 12:36:30 +0800

    fast2render 更新

--- src/util/utils.js
+++ src/util/utils.js
@@ -98,7 +98,7 @@ export const getAccount = () => {
 
 export const getLocalToken = (supplier) => {
   const params = {
-    elsAccount: supplier ? '是307201' : '307000',
+    elsAccount: supplier ? '307184' : '307000',
     elsSubAccount: '1001',
     elsSubAccountPassword: md5('123')
   };
